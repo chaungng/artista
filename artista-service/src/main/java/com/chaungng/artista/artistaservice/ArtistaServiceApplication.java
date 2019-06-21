e94d3db2c45a91310950363f4f4e9aaf4bd479ed
chaungng <ngocchau0710@hotmail.com>
2019-06-21 01:02:28 -0700

    commit initial packages

---
+++ artista-service/src/main/java/com/chaungng/artista/artistaservice/ArtistaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chaungng.artista.artistaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistaServiceApplication.class, args);
+	}
+
+}
---
+++ artista-service/src/test/java/com/chaungng/artista/artistaservice/ArtistaServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chaungng.artista.artistaservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArtistaServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
